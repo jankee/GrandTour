197f4c1d31131a004045534aa126da30bf60bee1
[회사]양정훈 <jjankee@gmail.com>
2016-03-28 21:02:01 +0900

    145페이지 까지

---
+++ GrandTour/Assets/02Scripts/FollowCam.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour
+{
+    //추적할 타겟 오브젝트
+    public Transform targetTr;
+    //카메라와 일정 거리
+    public float dist = 10f;
+    //카메라의 높이
+    public float height = 3f;
+    //부드러운 추적을 위한 값
+    public float dampTrace = 20f;
+
+    //카메라 자신의 Transform변수
+    private Transform camTr;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        camTr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        camTr.position = Vector3.Lerp(camTr.position, targetTr.position - (targetTr.forward * dist) +
+            (Vector3.up * height), Time.deltaTime * dampTrace);
+        print("HI");
+        camTr.LookAt(targetTr.position);
+	}
+}
---
+++ GrandTour/Assets/02Scripts/PlayerCtrl.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class Anim
+{
+    public AnimationClip idle;
+    public AnimationClip runForward;
+    public AnimationClip runBackWard;
+    public AnimationClip runRight;
+    public AnimationClip rumLeft;
+}
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private float h = 0f;
+    private float v = 0f;
+
+    private Transform tr;
+
+    public float moveSpeed = 10f;
+
+    public float rotSpeed = 100f;
+
+    //인스펙터뷰에 표시할 애니메이션 클레스 변수
+    public Anim anim;
+    //3D 모델의 Animation 컴포넌트에 접근하기 위한 변수
+    public Animation _animation;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Transform 컴포넌트 할당
+        tr = this.GetComponent<Transform>();
+
+        //자신하위의 Animation컴포넌트 찾아와 할당
+        _animation = GetComponentInChildren<Animation>();
+
+        _animation.clip = anim.idle;
+        _animation.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        //Translate(이동방향 * 속도 * 변위값 * Time.deltaTime. 기준좌표(로컬좌표))
+        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+
+        tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("MouseX"));
+
+        //키보드 입력값을 기준으로 애니메이션 실행
+        if (v >= 0.1f)
+        {
+            //_animation.clip = anim.runForward;
+            _animation.CrossFade(anim.runForward.name, 0.3f);
+        }
+        else if (v <= -0.1f)
+        {
+            _animation.CrossFade(anim.runBackWard.name, 0.3f);
+        }
+        else if (h >= 0.1f)
+        {
+            _animation.CrossFade(anim.runRight.name, 0.3f);
+        }
+        else if (h <= 0.1f)
+        {
+            _animation.CrossFade(anim.rumLeft.name, 0.3f);
+        }
+        else
+        {
+            _animation.CrossFade(anim.idle.name, 0.3f);
+        }
+
+        print("h = " + h + "\n" + "v = " + v);
+	}
+}
