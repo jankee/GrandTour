240eceb01d0951a531f836992d7e209f297f453f
[회사]양정훈 <jjankee@gmail.com>
2016-03-28 17:09:58 +0900

    탱크작업 멈춤

---
+++ GrandTour/Assets/Scripts/PlayerList.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerType
+{
+    Sprinter,
+    Climber,
+    AllRounder,
+    Rouleur,
+    TimeTrilist,
+}
+
+
+public class PlayerList : MonoBehaviour
+{
+    public PlayerType type;
+
+    public Sprite spriteNomal;
+
+    public Sprite spriteHighlight;
+
+    public int health;
+
+    public int power;
+
+    public int speed;
+
+    public int technic;
+
+    public int sprint;
+
+    public int sprintPoint;
+
+	public void Use()
+    {
+        switch (type)
+        {
+            case PlayerType.Sprinter:
+                print("I am Sprinter");
+                break;
+            case PlayerType.Climber:
+                print("I am Climber");
+                break;
+            case PlayerType.AllRounder:
+                break;
+            case PlayerType.Rouleur:
+                break;
+            case PlayerType.TimeTrilist:
+                break;
+        }
+    }
+}
---
+++ GrandTour/Assets/Scripts/PlayerTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerTest : MonoBehaviour
+{
+    public List<PlayerList> playerList;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        playerList = new List<PlayerList>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
