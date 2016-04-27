4981a3df5839bf8fe58084c37ad545f3a197adf3
[회사]양정훈 <jjankee@gmail.com>
2016-04-27 21:07:06 +0900

    589까지 작업함

--- TankPvP/Assets/02.Scripts/CannonCtrl.cs
+++ TankPvP/Assets/02.Scripts/CannonCtrl.cs
@@ -5,19 +5,57 @@ public class CannonCtrl : MonoBehaviour
 {
     private Transform tran;
 
+    //PhotonView 컴포넌트 변수
+    private PhotonView pv;
+
+    //원격 네트워크 탱크의 포신 회전 각도를 저장할 변수
+    private Quaternion currRot = Quaternion.identity;
+
     public float rotSpeed = 100f;
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
         tran = GetComponent<Transform>();
+        pv = GetComponent<PhotonView>();
+
+        //포톤뷰의 Observed 속성을 바꾸어줌
+        pv.ObservedComponents[0] = this;
+        //Photon View의 동기화 속성을 설정
+        pv.synchronization = ViewSynchronization.UnreliableOnChange;
+
+        //초기 회전 값
+        currRot = tran.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float angle = Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * rotSpeed;
+        if (pv.isMine)
+        {
+            float angle = Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * rotSpeed;
 
-        tran.Rotate(angle, 0, 0);
+            tran.Rotate(angle, 0, 0);
+        }
+        else
+        {
+            //현재 회전 각도에서 수신받은 실시간 회전 각도로 부드럽게 회전
+            tran.localRotation = Quaternion.Slerp(tran.localRotation, currRot, Time.deltaTime * 3f);
+        }
+        
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(tran.localRotation);
+            print("Cannon stream : " + stream);
+        }
+        else
+        {
+            currRot = (Quaternion)stream.ReceiveNext();
+            print("Cannon currRot : " + currRot);
+        }
     }
 }
--- TankPvP/Assets/02.Scripts/TurretCtrl.cs
+++ TankPvP/Assets/02.Scripts/TurretCtrl.cs
@@ -9,29 +9,65 @@ public class TurretCtrl : MonoBehaviour
     //터렛의 회전 속도
     public float turretSpeed = 5f;
 
+    //포톤뷰 컴포넌트 변수
+    private PhotonView pv = null;
+    //원격 네트워크 탱크의 터렛 회전값을 저장할 변수
+    private Quaternion currRot = Quaternion.identity;
+
+
     // Use this for initialization
-    void Start()
+    void Awake()
     {
         tran = GetComponent<Transform>();
+        pv = GetComponent<PhotonView>();
+
+        //Photon View의 Observed 속성을 이 스크립트로 지정
+        pv.ObservedComponents[0] = this;
+        //Photon View의 동기화 속성을 설정
+        pv.synchronization = ViewSynchronization.UnreliableOnChange;
+
+        currRot = tran.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //메인카메라에서 마우스 커서의 위치로 캐스팅되는 Ray를 생성
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //print(ray);
-        Debug.DrawLine(ray.origin, ray.direction * 100f, Color.red);
-
-        //Physics.Raycast(광선, out 충돌정보, 거리, 레이어 마스크)
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1<<8))
+        if (pv.isMine)
         {
-            //Ray에 맞는 위치를 로컬좌표로 변환
-            Vector3 relative = tran.InverseTransformPoint(hit.point);
-            //역탄젠트 함수인 Atan2로 두 점간의 각도를 계산
-            float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
-            //rotSpeed 변수에 지정된 속도로 회전
-            tran.Rotate(0, angle * Time.deltaTime * turretSpeed, 0);
+            //메인카메라에서 마우스 커서의 위치로 캐스팅되는 Ray를 생성
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //print(ray);
+            Debug.DrawLine(ray.origin, ray.direction * 100f, Color.red);
+
+            //Physics.Raycast(광선, out 충돌정보, 거리, 레이어 마스크)
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << 8))
+            {
+                //Ray에 맞는 위치를 로컬좌표로 변환
+                Vector3 relative = tran.InverseTransformPoint(hit.point);
+                //역탄젠트 함수인 Atan2로 두 점간의 각도를 계산
+                float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
+                //rotSpeed 변수에 지정된 속도로 회전
+                tran.Rotate(0, angle * Time.deltaTime * turretSpeed, 0);
+            }
+        }
+        //원격 네트워크 플레이어의 탱크일 경우
+        else
+        {
+            //회전각도에서 수신받은 실시간 회전각도 부드럽게 회전
+            tran.localRotation = Quaternion.Slerp(tran.localRotation, currRot, Time.deltaTime * 3f);
+        }
+    }
+
+    //송수신 콜백 함수
+    void OnPhotonSerializView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(tran.localRotation);
+        }
+        else
+        {
+            currRot = (Quaternion)stream.ReceiveNext();
         }
     }
 }
