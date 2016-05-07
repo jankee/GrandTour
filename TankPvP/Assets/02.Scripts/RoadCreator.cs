63cfb3f16351d24fe042f5c2243310c29c96ccfe
[집]양정훈 <jjankee@gmail.com>
2016-05-07 18:46:27 +0900

    작업 중

---
+++ TankPvP/Assets/02.Scripts/RoadCreator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadCreator : MonoBehaviour
+{
+    //기본이 되는 로드 오브젝트
+    public GameObject roadObj;
+
+    //도로의 갯수
+    public int roadNum;
+
+    //배열로 묶을 변수
+    private GameObject[] roadObjs = null;
+
+    public void RoadBuilder()
+    {
+        roadObjs = new GameObject[roadNum];
+
+        for (int i = 0; i < roadNum; i++)
+        {
+            roadObjs[i] = (GameObject)Instantiate(roadObj, new Vector3(0, 0, 0), Quaternion.identity);            
+        }
+
+        for (int i = 0; i < roadObjs.Length; i++)
+        {
+            if (i == 0)
+            {
+                roadObjs[i].transform.position = Vector3.zero;
+            }
+            else
+            {
+                roadObjs[i].transform.position = roadObjs[i - 1].transform.FindChild("EndPoint").position;
+            }
+            
+        }
+
+        print("Create Road");
+    }
+
+}
---
+++ TankPvP/Assets/02.Scripts/RoadCreatorEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(RoadCreator))]
+public class RoadCreatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RoadCreator myScript = (RoadCreator)target;
+        if (GUILayout.Button("생성"))
+        {
+            myScript.RoadBuilder();
+        }
+    }
+}
